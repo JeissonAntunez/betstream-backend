497ac42ffc13897ab4ee53384d34d3ad7a814e0a
JeissonAntunez <jeissonantunez@gmail.com>
2026-04-24 00:06:33 -0500

    Lombok

---
+++ src/main/java/com/Learning/BetStreamApplication.java
@@ -0,0 +1,20 @@
+package com.Learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.r2dbc.R2dbcAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
+import org.springframework.boot.autoconfigure.amqp.RabbitAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication(exclude = {
+        R2dbcAutoConfiguration.class,
+        RedisAutoConfiguration.class,
+        RabbitAutoConfiguration.class,
+        SecurityAutoConfiguration.class
+})
+public class BetStreamApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BetStreamApplication.class, args);
+    }
+}
